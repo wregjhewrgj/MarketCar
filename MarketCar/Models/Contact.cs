451e64bbe08e0e0cc3f584826181c1a12c119b9c
DonerFox <107530095+wregjhewrgj@users.noreply.github.com>
2023-11-11 14:13:43 +1000

    first commit
    first commit

---
+++ MarketCar/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations; // исправленное пространство имён
+namespace MarketCar.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите Имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите почту")]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
